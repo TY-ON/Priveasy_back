1d67a9faa8346d03f2f475371756461d81dc5b65
TY-ON <ty05071@naver.com>
2025-02-13 18:58:09 +0900

    make dockerfile & docker-compose / edit crawl.ts using cheerio

--- src/routes/utils/crawl.ts
+++ src/routes/utils/crawl.ts
@@ -1,10 +1,15 @@
 const puppeteer = require('puppeteer');
-const jsdom = require("jsdom")
-const { JSDOM } = jsdom
-global.DOMParser = new JSDOM().window.DOMParser
+import * as cheerio from 'cheerio';
 
 export async function getData(url: string): Promise<string>{
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null,
+        args: [
+            '--no-sandbox',
+            '--disable-gpu',
+        ],
+     });
 	const page = await browser.newPage();
 	await page.goto(url);
 	await page.waitForFunction(
@@ -25,34 +30,46 @@ export async function crawlPrivacy(url: string): Promise<string> {
         return "failed";
     }
 
-    const html_dom = new DOMParser().parseFromString(data, 'text/html');
+    const html = cheerio.load(data);
     let href: string | undefined = undefined;
 
-    function findPrivacyLink(element: Element | Document): string | undefined {
-        const links = element.querySelectorAll<HTMLAnchorElement>("a[href]");
-        for (const link of links) {
-            if (/개인.*정보.*처리.*(방침|약관)/.test(link.textContent || "")) {
-                return link.href;
+    function findPrivacyLink(element: cheerio.Cheerio): string | undefined {
+        for (const link of element.find("a")) {
+            if (/개인.*정보.*처리.*(방침|약관)/.test(html(link).text() || "")) {
+                return html(link).attr('href');
             }
         }
         return undefined;
     }
 
     // 1. 푸터에서 먼저 검색
-    const footer = html_dom.querySelector("footer");
+    const footer = html("footer");
     if (footer) {
         href = findPrivacyLink(footer);
     }
 
     // 2. 푸터에서 못 찾으면 전체 페이지 검색
     if (!href) {
-        href = findPrivacyLink(html_dom);
+        href = findPrivacyLink(html('html'));
     }
 
     if (href) {
         console.log(`Found Privacy Policy: ${href}`);
         const privacy = await getData(href);
-        return privacy;
+        
+        const $ = cheerio.load(privacy);
+        $('head').remove();
+        $('script').remove();
+        $('style').remove();
+        $('#header').remove();
+        $('.header').remove();
+        $('#footer').remove();
+        $('.footer').remove();
+        const privacyContent = $('body').text();
+        if (privacyContent){
+            return privacyContent;
+        }
+        return "failed";
     }
 
     return "failed";
